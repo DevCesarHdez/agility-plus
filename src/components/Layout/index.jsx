10c214d622aaaba71ce34b308d560ab81a5efc5f
Cesar Hernandez <dev.cesar.hdez@gmail.com>
2023-07-21 17:39:15 +0000

    Grid para las tarjetas

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,7 +2,7 @@ import Card from "../Card/Card";
 
 const Layout = ({children}) => {
 	return <>
-		<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+		<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 			{children}
 		</div>
 	</>
