62afd60df1ee6526a7546558c4490bd6998c76fa
Cesar Hernandez <dev.cesar.hdez@gmail.com>
2023-07-21 17:40:14 +0000

    Cambio de nombre de props status -> initialStatus

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -3,9 +3,9 @@ import Card from "../components/Card/Card"
 
 const Cards = () => {
 	return <Layout>
-		<Card name="Cesar" status="Alive" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-		<Card name="Cesar" status="Alive" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-		<Card name="Cesar" status="Alive" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+		<Card name="Cesar" initialStatus="Alive" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+		<Card name="Cesar" initialStatus="Dead" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+		<Card name="Cesar" initialStatus="unknown" species="Humman" imgAvatar="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
 	</Layout>
 }
 
